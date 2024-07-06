8c82010da44365be8d5247e2203e9d42e563ca11
Mostafa0Alii <mostafa0alii@gmail.com>
2024-07-06 05:48:59 +0300

    finish sliders

---
+++ app/Http/Controllers/Dashboard/Admin/SliderController.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Http\Controllers\Dashboard\Admin;
+use App\Models\Slider;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\{DB};
+class SliderController extends Controller {
+    public function index() {
+        $sliders = Slider::orderBy('id', 'DESC')->get();
+        return view('dashboard.admin.sliders.index', compact('sliders'));
+    }
+
+    public function store(Request $request) {
+        //try {
+            $slider = Slider::create($request->input());
+            if ($request->hasFile('image')) {
+                $fileName = 'slider-' . time() . '.' . $request->file('image')->getClientOriginalExtension();
+                $slider->storeImage($request->file('image')->storeAs('slider', $fileName, 'upload_image'));
+            }
+            toastr()->success('تم الحفظ بنجاح');
+            return redirect()->route('sliders.index');
+        /*} catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => 'حدث خطا حاول مره اخرى']);
+        }*/
+    }
+
+    public function update(Request $request, Slider $slider) {
+        try {
+            $slider->update($request->input());
+            if ($request->hasFile('image')) {
+                $fileName = 'slider-' . time() . '.' . $request->file('image')->getClientOriginalExtension();
+                $slider->updateImage($request->file('image')->storeAs('slider', $fileName, 'upload_image'));
+            }
+            toastr()->success('تم التحديث بنجاح ');
+            return redirect()->route('sliders.index');
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => 'حدث خطا حاول مره اخرى']);
+        }
+    }
+
+    public function destroy(string $id) {
+        try {
+            DB::beginTransaction();
+            $slider = Slider::findOrFail($id);
+            $slider->delete();
+            $slider->deleteImage();
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->withErrors(['error' => 'حدث خطا حاول مره اخرى']);
+        }
+        toastr()->success('تم الحذف بنجاح');
+        return redirect()->route('sliders.index');
+    }
+}
---
+++ resources/views/dashboard/admin/sliders/modal/edit.blade.php
@@ -0,0 +1,51 @@
+<!-- edit_modal_Grade -->
+<div class="modal fade" id="edit{{ $slider->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 style="font-family: 'Cairo', sans-serif;" class="modal-title" id="exampleModalLabel">
+                    تعديل {{ $slider?->name }}
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- edit_form -->
+                <form action="{{route('sliders.update', $slider->id)}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="row">
+                        <div class="col">
+                            <label for="name" class="mr-sm-2">الاسم :</label>
+                            <input id="name" type="text" name="name" class="form-control" value="{{ $slider?->name }}">
+                        </div>
+                        <div class="col">
+                            <label for="is_active" class="mr-sm-2">الحاله :</label>
+                            <div class="box">
+                                <select class="form-control" name="status">
+                                    <option value="1" {{ (old('status', $slider->status) == '1') ? 'selected' : '' }} >فعال</option>
+                                    <option value="0" {{ (old('status', $slider->status) == '0') ? 'selected' : '' }} >غير فعال</option>
+                                </select>
+                                @error('status')<span class="text-danger">{{ $message }}</span>@enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>الصوره : <span style="color:rgb(199, 8, 8)">*</span></label>
+                        <input class="form-control img" name="image" type="file" accept="image/*" onchange="previewImage(this);">
+                        <img class="img-thumbnail img-fluid" id="image-preview" src="{{$slider->ImagePath()}}" alt="{{$slider->name}}">
+                        
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary"
+                                data-dismiss="modal">اغلاق</button>
+                        <button type="submit"
+                                class="btn btn-primary">تحديث</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
